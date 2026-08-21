37c34974bd70c859ea42bf13b4f9bf2a700c90e7
Kleyton Ferreira <kleytondev40@gmail.com>
2026-08-21 15:13:44 -0300

    feat: add table

---
+++ app/products/_components/table-columns.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Badge } from "@/app/_components/ui/badge";
+import { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { CircleIcon } from "lucide-react";
+
+const getStatusLabel = (product: Product) => {
+  if (product.stock === 0) {
+    return "Fora de estoque";
+  }
+  if (product.status === "IN_STOCK") {
+    return "Em estoque";
+  }
+};
+
+export const productTablecolumns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: () => (
+      <div className="text-base font-semibold text-textColor-primary">
+        <h3>Produto</h3>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "price",
+    header: () => (
+      <div className="text-base font-semibold text-textColor-primary">
+        <h3>Valor unitário</h3>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "stock",
+    header: () => (
+      <div className="text-base font-semibold text-textColor-primary">
+        <h3>Estoque</h3>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "status",
+    header: () => (
+      <div className="text-base font-semibold text-textColor-primary">
+        <h3>Status</h3>
+      </div>
+    ),
+    cell: (row) => {
+      const product = row.row.original;
+      const label = getStatusLabel(product);
+      return (
+        <Badge
+          variant={label === "Em estoque" ? "destructive" : "outline"}
+          className="gap-1.5"
+        >
+          <CircleIcon
+            size={10}
+            className={`${label === "Em estoque" ? "fill-primary-foreground" : "fill-foreground"}`}
+          />
+          {label}
+        </Badge>
+      );
+    },
+  },
+];
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,35 @@
-const ProductsPage = () => {
+import { DataTable } from "../_components/ui/data-table";
+import { productTablecolumns } from "./_components/table-columns";
+import { db } from "../_lib/prisma";
+import { PlusIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+
+const ProductsPage = async () => {
+  const products = await db.product.findMany({});
+
   return (
-    <div className="pt-8">
-      <h1>Products-page( "!" )</h1>
+    <div className="w-full p-6">
+      <div className="mb-5 flex items-center justify-between">
+        <div>
+          <h2 className="text-sm font-semibold text-textColor-primary">
+            Produtos
+          </h2>
+          <h3 className="text-xl font-semibold text-textColor-third">
+            Gestão de produtos
+          </h3>
+        </div>
+        <div>
+          <Button variant="destructive" className="text-base [&_svg]:size-auto">
+            <PlusIcon size={18} /> Novo produto
+          </Button>
+        </div>
+      </div>
+      <div>
+        <DataTable
+          columns={productTablecolumns}
+          data={JSON.parse(JSON.stringify(products))}
+        />
+      </div>
     </div>
   );
 };
