ccba4a2b1f14d7d5aceffdc3070e0e9b2dd60768
Kleyton Ferreira <kleytondev40@gmail.com>
2026-08-25 17:59:28 -0300

    feat: ensure sale creation is done with a transaction

--- app/_actions/sale/create-sale/index.ts
+++ app/_actions/sale/create-sale/index.ts
@@ -6,50 +6,54 @@ import { revalidatePath } from "next/cache";
 
 export const CreateSale = async (data: CreateSaleSchema) => {
     createSaleSchema.parse(data)
-    const sale = await db.sale.create({
-        data: {
-            date: new Date()
-        }
-    })
 
-    for (const product of data.products) {
+    await db.$transaction(async (trx) => {
 
-        const productFromDb = (
-            await db.product.findUnique({
-                where: {
-                    id: product.id
+        const sale = await db.sale.create({
+            data: {
+                date: new Date()
+            }
+        })
+
+        for (const product of data.products) {
+
+            const productFromDb = (
+                await db.product.findUnique({
+                    where: {
+                        id: product.id
+                    }
+                })
+            )
+
+            if (!productFromDb) {
+                throw new Error("Product not found")
+            }
+
+            const productIsOutOfStock = product.quantity > productFromDb.stock
+            if (productIsOutOfStock) {
+                throw new Error("Product out of stock")
+            }
+
+            await trx.saleProduct.create({
+                data: {
+                    saleId: sale.id,
+                    productId: product.id,
+                    quantity: product.quantity,
+                    unitPrice: productFromDb.price
                 }
             })
-        )
-
-        if (!productFromDb) {
-            throw new Error("Product not found")
-        }
-
-        const productIsOutOfStock = product.quantity > productFromDb.stock
-        if (productIsOutOfStock) {
-            throw new Error("Product out of stock")
-        }
-
-        await db.saleProduct.create({
-            data: {
-                saleId: sale.id,
-                productId: product.id,
-                quantity: product.quantity,
-                unitPrice: productFromDb.price
-            }
-        })
-        await db.product.update({
-            where: {
-                id: product.id
-            },
-            data: {
-                stock: {
-                    decrement: product.quantity
+            await trx.product.update({
+                where: {
+                    id: product.id
+                },
+                data: {
+                    stock: {
+                        decrement: product.quantity
+                    }
                 }
-            }
-        })
-    }
+            })
+        }
+    })
     revalidatePath("/products")
 
 }
